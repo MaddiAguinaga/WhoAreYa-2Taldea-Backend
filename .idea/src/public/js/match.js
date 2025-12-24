1a657fcf5270e1830eb56bd17cace6600ed841cc
Xucin <158266466+Xucin2218@users.noreply.github.com>
2025-12-24 12:24:33 +0100

    Delete .idea directory

--- .idea/src/public/js/match.js
+++
@@ -1,6 +0,0 @@
-export function match(query, text) {
-    if (!query || !text) return false;
-
-    // lowercase konparaketa azpikateak bilatzeko
-    return text.toLowerCase().includes(query.toLowerCase());
-}
