41be25ff939b7652f9eba061bad81f69605a746a
Xabier Ucin <xucin002@ikasle.ehu.eus>
2025-12-31 13:55:25 +0100

    register/login/logout implementazioa

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-
 import express from "express";
 import path from "path";
+import session from "express-session";
+import authRoutes from "./routes/api/auth.routes.js";
 
 const app = express();
 
@@ -8,18 +9,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// ➕ SESSION (oso sinplea, milestone-rako nahikoa)
+app.use(
+    session({
+       secret: "secret",
+       resave: false,
+       saveUninitialized: false
+    })
+);
+
 // Static files
 app.use(express.static(path.join(process.cwd(), "src", "public")));
 
-
+// AUTH ROUTES
+app.use("/api/auth", authRoutes);
 
 // Proba route
-
 app.get("/", (req, res) => {
    res.send("WhoAreYa Backend is running");
 });
-
-
-
 
 export default app;
